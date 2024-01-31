50eb50d280cb4b5c748e70ebe49bd999b85f89e5
edi vidrascu <edividrascu@gmail.com>
2024-01-31 19:18:43 +0100

    ejercicio 3 components

--- componentes/src/App.jsx
+++ componentes/src/App.jsx
@@ -1,4 +1,6 @@
 
+
+import Message from "./Message";
 import { HelloWorld } from "./HelloWorld";
 
 export function App() {
@@ -7,6 +9,16 @@ export function App() {
       <h1>My awesome react</h1>
       <hr />
       <HelloWorld />
+      <HelloWorld />
+      <HelloWorld />
+      <HelloWorld />
+      <HelloWorld />
+      <Message />
     </div>
   );
 }
+// Sí, se puedes usar el componente HelloWorld más de una vez en el componente App. 
+// Cada instancia del componente HelloWorld se renderizará de forma independiente.
+
+// Sí,se puedes renderizar el componente Message directamente dentro del componente App.
+// Esto mostrará el mensaje "¡Qué hermoso día!" directamente 
