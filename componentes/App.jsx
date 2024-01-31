c24a80fb2e2d959c9099943336dbb4e005274276
edi vidrascu <edividrascu@gmail.com>
2024-01-31 18:21:34 +0100

    COMPONENTS

---
+++ componentes/App.jsx
@@ -0,0 +1,11 @@
+import { HelloWorld } from "./src/HelloWorld";
+
+export function App(){
+    return(
+        <div>
+            <h1>My awesome react</h1>
+            <hr />
+            <HelloWorld/>
+        </div>
+    )
+}
