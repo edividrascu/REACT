e8bceec96ac521c27d3db63219fa6dfc7b08a180
edi vidrascu <edividrascu@gmail.com>
2024-02-15 09:58:08 +0100

    SWR - 2

--- componentes/src/useGithubUser.jsx
+++ componentes/src/useGithubUser.jsx
@@ -1,13 +1,18 @@
-import useSWR from "swr"
+import useSWR from "swr";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export function useGithubUser(username) {
+  
+  if (username === null) {
+    return { data: null, error: "Nombre de usuario nulo" };
+  }
 
   
-  const {data,error,isLoading} = useSWR(`https://api.github.com/users/${username}`,fetcher)
+  const { data, error, isLoading } = useSWR(`https://api.github.com/users/${username}`, fetcher);
 
-  console.log(isLoading)
+ 
+  console.log(isLoading);
 
-  return {data, error}
-  }
+  return { data, error };
+}
